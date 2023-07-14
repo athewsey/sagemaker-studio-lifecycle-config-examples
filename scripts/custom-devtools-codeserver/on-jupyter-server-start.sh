b34b762926c3b748179500df8be2dae6a2c1ac45
Alex Thewsey <thewsey@amazon.com>
2023-07-14 15:18:45 +0800

    fix(custom): First run rm error on devtools
    Fix issue where the devtools-* scripts would fail on first run as
    rm would try to remove a symlink file that does not exist yet.

--- scripts/custom-devtools-codeserver/on-jupyter-server-start.sh
+++ scripts/custom-devtools-codeserver/on-jupyter-server-start.sh
@@ -180,7 +180,7 @@ jupyter labextension disable jupyterlab-server-proxy
 # Improve autocomplete source links by symlinking /opt packages from local folder and allowing
 # opening of these symlinked files (outside the user home directory):
 mkdir -p .lsp_symlink
-rm .lsp_symlink/opt
+rm -f .lsp_symlink/opt
 ln -s /opt .lsp_symlink/opt
 echo yes | jupyter server --generate-config
 sed -i '1i c.ContentsManager.allow_hidden = True' .jupyter/jupyter_server_config.py
